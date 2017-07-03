22103c8bdd94c7af8338c568d443a648a0f44932
Pascal Lando <pascal.lando@u-picardie.fr>
2017-07-03 11:26:16 +0200

    Fixed version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'victorpy',
     packages = ['victorpy'],
-    version = '0.1',
+    version = 'v0.1',
     description = 'A simple yet powerful static site generator with Python sugar',
     author = 'Pascal LANDO',
     author_email = 'pascal.lando@u-picardie.fr',
